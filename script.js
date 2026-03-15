3ebcbcb5637200a84616f5695ad17011088e8183
Vansh Sharma <vanshsharma.14.2005@gmail.com>
2026-03-15 15:59:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    fetch('/data.json')
+    fetch('data.json')
         .then(response => response.json())
         .then(data => {
             renderApp(data);
